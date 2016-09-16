77b275505106923e669a6cf3a7d61fcc85c63fcf
Satoshi Suzuki <sat.suzuki@uec.ac.jp>
2016-09-17 01:57:38 +0900

    maybe i'm tired

--- jnns_1.sh
+++ jnns_1.sh
@@ -27,14 +27,13 @@ sed -i -e "s/num_output: 1000/num_output: ${CLASS}/g" ${DIR}/train_val.prototxt
 sed -i -e "s@examples/imagenet/ilsvrc12_train_lmdb@${TRAIN}@g" ${DIR}/train_val.prototxt
 sed -i -e "s@examples/imagenet/ilsvrc12_val_lmdb@${TEST}@g" ${DIR}/train_val.prototxt
 sed -i -e "s@data/ilsvrc12/imagenet_mean.binaryproto@${MEAN}@g" ${DIR}/train_val.prototxt
-sed -i -e "s/batch_size: 256/batch_size: 64/g"
+sed -i -e "s/batch_size: 256/batch_size: 64/g" ${DIR}/train_val.prototxt
 #solver.prototxtも一部改造
 sed -i".org" -e "s/GPU/CPU/g" ${DIR}/solver.prototxt
 sed -i -e "s/base_lr: 0.01/base_lr: 0.005/g" ${DIR}/solver.prototxt
 sed -i -e "s@models/bvlc_reference_caffenet/caffenet_train@transfer_caffenet@g" ${DIR}/solver.prototxt
 sed -i -e "s@test_iter: 1000@test_iter: 12@g" ${DIR}/solver.prototxt
 sed -i -e "s@models/bvlc_reference_caffenet/train_val.prototxt@${DIR}/train_val.prototxt@g" ${DIR}/solver.prototxt
-sed -i -e ""
 
 #学習開始
 sudo build/tools/caffe train -solver ${DIR}/solver.prototxt -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
