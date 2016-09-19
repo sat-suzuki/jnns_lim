3184b1713de34e0b598b4c76d7ce9391cfb422c7
Satoshi Suzuki <sat.suzuki@uec.ac.jp>
2016-09-19 20:02:28 +0900

    modify

--- feature.py
+++ feature.py
@@ -1,34 +1,28 @@
 import sys, os, os.path
 import numpy as np
-sys.path.insert(0,"../python/")
 import caffe
 
-caffe_root = '../' 
-MEAN_FILE = '../python/caffe/imagenet/ilsvrc_2012_mean.npy'
-MODEL_FILE = 'train_val.prototxt'
-PRETRAINED = '/home/satoshi/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
-LAYER = 'fc7wi'
+caffe_root = '../'
+sys.path.insert(0, caffe_root + "python/")
+MEAN_FILE = caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'
+MODEL_FILE = 'deploy.prototxt'
+PRETRAINED = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+LAYER = 'fc6wi'
 INDEX = 4
 
 net = caffe.Classifier(MODEL_FILE, PRETRAINED)
-net.set_mode_cpu()
-net.set_mean('data', np.load(MEAN_FILE))
-net.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'))
+#net.set_phase_test()
+caffe.set_mode_cpu()
+#net.set_mode_gpu()
+net.transformer.set_mean('data', np.load(MEAN_FILE))
+#net.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'))
+net.transformer.set_raw_scale('data', 255)
+net.transformer.set_channel_swap('data', (2,1,0))
+
+image = caffe.io.load_image(caffe_root + '/examples/images/cat.jpg')
+net.predict([ image ])
+feat = net.blobs[LAYER].data[INDEX].flatten().tolist()
+print(' '.join(map(str, feat)))
 
 
-f = open('test.txt')
-data1 = f.read()
-f.close()
-lines1 = data1.split('\n')
-i = 0
-for i in range(len(lines1)-1):
-    image = caffe.io.load_image(lines1[i])
-    net.predict([ image ])
-    feat = net.blobs[LAYER].data[INDEX].flatten().tolist()
-    l2_feat = feat/np.linalg.norm(feat)
-    if i<100:
-        print "0",
-        print(' '.join(map(str, l2_feat)))
-    else:
-        print "1",
-        print(' '.join(map(str, l2_feat)))
+
